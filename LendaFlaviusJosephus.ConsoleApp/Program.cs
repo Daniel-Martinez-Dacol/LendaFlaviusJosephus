3ca7bb09fce9e83c6dcaf12829f45933e00d820a
danie <danie@DESKTOP-76DAUAO>
2022-02-18 16:20:46 -0300

    Add project files.

---
+++ LendaFlaviusJosephus.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LendaFlaviusJosephus.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero de casos: ");
+            int quantidadeDeCasos = int.Parse(Console.ReadLine());
+            int caso = 0;
+
+            do
+            {
+                caso++;
+                int eliminacoes = 0;
+                Console.Write("\nDigite o numero de pessoas dentro do grupo: ");
+                int numeroDePessoasDentroDoCirculo = int.Parse(Console.ReadLine());
+                
+
+                Console.Write("\nDigite o numero de saltos dentro do grupo: ");
+                int saltoDeUmHomemAteOutro = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    if (numeroDePessoasDentroDoCirculo % saltoDeUmHomemAteOutro != 0)
+                    {
+                        eliminacoes++;
+                        numeroDePessoasDentroDoCirculo--;
+                    }
+
+                } while (numeroDePessoasDentroDoCirculo == 1);
+                
+                    
+                       
+                    
+                
+
+                
+
+                Console.WriteLine($"Caso {caso} : {eliminacoes}");
+                
+            } while (caso != quantidadeDeCasos);
+
+            
+
+        }
+    }
+}
